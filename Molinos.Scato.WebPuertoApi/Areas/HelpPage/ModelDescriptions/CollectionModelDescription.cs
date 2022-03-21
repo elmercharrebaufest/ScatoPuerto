64f3dc109b046f70daa45d159aacf6f69c28d46f
Lucas Manuel Olivella <lmolivella@baufest.com>
2022-03-21 11:54:24 +0000

    Eliminado Molinos.Scato.WebPuertoApi

--- Molinos.Scato.WebPuertoApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Molinos.Scato.WebPuertoApi.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Molinos.Scato.WebPuertoApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Molinos.Scato.WebPuertoApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
