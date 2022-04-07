f196eb6c69846620abf5738de2c41fe5bdb12853
Anthony Portilla Cano <aportilla@baufest.com>
2022-04-07 11:40:25 -0500

    Se el vance de validar ver mapa y no encontro imo

--- Molinos.Scato.WebPuerto/src/app/modulos/lineup/embarque/lineup-embarque.component.ts
+++ Molinos.Scato.WebPuerto/src/app/modulos/lineup/embarque/lineup-embarque.component.ts
@@ -36,6 +36,8 @@ export class LineupEmbarqueComponent implements OnInit {
   permisosScato: typeof PermisosScato = PermisosScato;
   posicionesDeLineUps: number[];
   embarquesPuerto: InstanciaWorkflowPuerto[];
+  hayBuque=true;
+  mensajeBuque:string;
   private user: Usuario;
   constructor(
     private lineUpService: LineupService,
@@ -54,10 +56,11 @@ export class LineupEmbarqueComponent implements OnInit {
       this.fechaCarta = formatDate(this.instanciaWorkflow.lineUp.cartaDeSubidaAprobada, 'yyyy-MM-dd', 'es-ar');
       this.horaCarta = formatDate(this.instanciaWorkflow.lineUp.cartaDeSubidaAprobada, 'HH:mm', 'es-ar');
     }
+    this.mensajeBuque=this.hayBuque?"Ver en el mapa":"No se encontró. Completar IMO";
     this._procesoService.disposeData();
     this.embarquesPuerto = this.observador != null ? this.observador.ListarEmbarques().filter(u => u.embarque.vicentin == this.instanciaWorkflow.embarque.vicentin && u.embarque.noryon == this.instanciaWorkflow.embarque.noryon && u.embarque.sanBenito == this.instanciaWorkflow.embarque.sanBenito && u.embarque.otrosMuelles == this.instanciaWorkflow.embarque.otrosMuelles) : [];
     this.posicionesDeLineUps = Array.from({ length: this.embarquesPuerto.length }, (v, k) => k + 1);
-    this.lineUpService.obtenerListadoUbicacionDeBuquePuerto().subscribe(res => { this.ubicacionDeBuquePuerto = res; });
+    this.lineUpService.obtenerListadoUbicacionDeBuquePuerto().subscribe(res => { this.ubicacionDeBuquePuerto = res;});
 
     this.lineUpService.obtenerListadoUbicacionDeBuquePuerto()
       .subscribe(res => {
