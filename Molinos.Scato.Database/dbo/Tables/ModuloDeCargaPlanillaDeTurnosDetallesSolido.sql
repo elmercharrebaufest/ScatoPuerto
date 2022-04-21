8b79b5486ec3d3b27acec027b9237812548bc9a2
lmolivella <lmolivella@baufest.com>
2022-04-21 11:35:26 -0300

    Cambios Planilla

--- Molinos.Scato.Database/dbo/Tables/ModuloDeCargaPlanillaDeTurnosDetallesSolido.sql
+++ Molinos.Scato.Database/dbo/Tables/ModuloDeCargaPlanillaDeTurnosDetallesSolido.sql
@@ -8,6 +8,7 @@
     [MaterialPuerto_Id]                         INT NOT NULL,
     [Destino_Id]                                INT NOT NULL,
     [Cantidad]                                  DECIMAL(18, 8),
+    [FechaCarga] DATETIME NULL, 
     CONSTRAINT [PK_dbo.ModuloDeCargaPlanillaDeTurnosDetallesSolido] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_dbo.ModuloDeCargaPlanillaDeTurnosDetallesSolido_dbo.ModuloDeCargaPlanillaDeTurnos_MDCPlanillaDeTurnos_Id] FOREIGN KEY ([ModuloDeCargaPlanillaDeTurnos_Id]) REFERENCES [dbo].[ModuloDeCargaPlanillaDeTurnos] ([Id]) on delete cascade,
     CONSTRAINT [FK_dbo.ModuloDeCargaPlanillaDeTurnosDetallesSolido_dbo.Exportador_Exportador_Id] FOREIGN KEY ([Exportador_Id]) REFERENCES [dbo].[Exportador] ([Id]),
