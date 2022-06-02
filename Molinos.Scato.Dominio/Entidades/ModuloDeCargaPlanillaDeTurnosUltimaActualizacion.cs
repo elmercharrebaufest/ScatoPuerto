bf6473757cf7ab1acdcaa887b7cd222a6436249d
Lucas Olivella <lmolivella@baufest.com>
2022-06-02 10:57:51 -0300

    Cambios Fix Tablero

---
+++ Molinos.Scato.Dominio/Entidades/ModuloDeCargaPlanillaDeTurnosUltimaActualizacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Molinos.Scato.Dominio.Entidades
+{
+    public class ModuloDeCargaPlanillaDeTurnosUltimaActualizacion : IIdentificable
+    {
+        [Key]
+        public virtual int Id { get; set; }
+        public virtual int Carga_Id { get; set; }
+        public virtual ModuloDeCarga ModuloDeCarga { get; set; }
+        public virtual DateTime? Fecha { get; set; }
+
+
+    }
+}
