d7e55293d4009333a81525c26672d6358cb5d7b8
Martin MOA <mtrionfetti@baufest.com>
2022-06-02 11:17:30 -0300

    Logs test QA

--- Molinos.Scato.WebPuerto/src/app/modulos/carga/carga.component.ts
+++ Molinos.Scato.WebPuerto/src/app/modulos/carga/carga.component.ts
@@ -21,7 +21,7 @@ export class CargaComponent implements OnInit, OnDestroy {
   embarqueSelected: EmbarqueNav;
   unsubscribe: Subject<any>;
   embarqueSelectedEnLocalStorage: EmbarqueNav;
-  
+  LogCount: number = 0;
   @ViewChild(PlanoContentComponent, { static: false }) planoContent: PlanoContentComponent;
 
   constructor(
@@ -32,10 +32,21 @@ export class CargaComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    let actualDate = new Date();
+    let function_name = 'SECCIÓN OPERACIONES (CARGA)';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
     this.subscribeEmbarques();
+    function_name = 'SECCIÓN OPERACIONES (CARGA) - FIN';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
   }
 
   subscribeEmbarques() {
+    let actualDate = new Date();
+    let function_name = 'subscribeEmbarques';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
     this._procesoService.disposeData();
     try {
       this.workflowService.listarEmbarquesEnLineUp()
@@ -49,39 +60,84 @@ export class CargaComponent implements OnInit, OnDestroy {
       console.log(e);
       console.log("Error en listarEmbarquesEnLineUp");
     }
+    function_name = 'subscribeEmbarques - FIN';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
   }
 
   showPlano(event: boolean) {
+    let actualDate = new Date();
+    let function_name = 'showPlano';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
     this.embarqueSelected = this._procesoService.getEmbarqueSelected();
     setTimeout(() => {
       this.mostrarPlano = event;
     }, 50);
+    function_name = 'showPlano - FIN';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
   }
 
   showCargas(event) {
+    let actualDate = new Date();
+    let function_name = 'showCargas';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
     this.mostrarCargas = event;
+    function_name = 'showCargas - FIN';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
   }
 
   hideSpinner(event) {
+    let actualDate = new Date();
+    let function_name = 'hideSpinner';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
     setTimeout(() => {
       this.mostrarSpinner = event;
     }, 50);
+    function_name = 'hideSpinner - FIN';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
   }
 
   changeEmbarque() {
+    let actualDate = new Date();
+    let function_name = 'changeEmbarque';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
     this.mostrarCargas = false;
     this.mostrarSpinner = true;
     this.embarqueSelected = this._procesoService.getEmbarqueSelected();
 
     this.grabarEmbarqueSelectedEnLocalStorage( this.embarqueSelected );
+    function_name = 'changeEmbarque - FIN';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
   }
 
   grabarEmbarqueSelectedEnLocalStorage( embarqueSelected: EmbarqueNav ){
+    let actualDate = new Date();
+    let function_name = 'grabarEmbarqueSelectedEnLocalStorage';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
     localStorage.setItem("embarqueSelected", JSON.stringify(embarqueSelected));
+    function_name = 'grabarEmbarqueSelectedEnLocalStorage - FIN';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
   }
 
   obtenerEmbarqueSelectedEnLocalStorage(): EmbarqueNav {
+    let actualDate = new Date();
+    let function_name = 'obtenerEmbarqueSelectedEnLocalStorage';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
     let embarqueSelected = JSON.parse( localStorage.getItem("embarqueSelected") );
+    function_name = 'obtenerEmbarqueSelectedEnLocalStorage - FIN';
+    console.log("(" + ++this.LogCount + ")" + function_name + ":" + actualDate.getUTCHours() + ":" +actualDate.getUTCMinutes()  + ":" + actualDate.getUTCSeconds()  + "." + actualDate.getUTCMilliseconds())
+     
     return embarqueSelected;
   }
 
