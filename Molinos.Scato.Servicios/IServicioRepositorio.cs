a7b20839aea7d6afad6d52d4b8b47fcbbfec6792
Lucas Olivella <lmolivella@baufest.com>
2022-07-14 09:35:40 -0300

    fix iservicio duplicado

--- Molinos.Scato.Servicios/IServicioRepositorio.cs
+++ Molinos.Scato.Servicios/IServicioRepositorio.cs
@@ -2655,7 +2655,9 @@ namespace Molinos.Scato.Servicios
         void EliminarObservacionDeCalidad(int observacion_id);
 
         [OperationContract]
-        IList<BanderaDto> ObtenerBanderas();
+        void GenerarLogging(string service, string data, string tipo);
+        [OperationContract]
+        IList<BanderaDto> ObtenerBanderas(); 
 
         [OperationContract]
         void GuardarReciboDeBuque(int idEmbarque, ReciboDeBuqueDto reciboDeBuque);
