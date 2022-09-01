84e822ca8f23f96ed54a452a3cb3c7bae682a29d
Lucas Olivella <lmolivella@baufest.com>
2022-09-01 10:21:01 -0300

    Cambios Estado Buque

--- Molinos.Scato.Database/dbo/Tables/Embarque.sql
+++ Molinos.Scato.Database/dbo/Tables/Embarque.sql
@@ -35,7 +35,7 @@
     [Puntal] DECIMAL(18,2) not NULL default 0,
     [FechaLibrePlatica] DATETIME NULL, 
     [HoraLibrePlatica] NVARCHAR(8) NULL,
-    [EstadoBuque_Id] INT NOT NULL DEFAULT 1, 
+    [EstadoBuque_Id] INT NULL , 
     [filePathShipParticular] NVARCHAR(MAX) NULL, 
     [shipParticularArchivoNombre] NVARCHAR(MAX) NULL, 
     [FechaHoraInicioCarga] DATETIME NULL , 
