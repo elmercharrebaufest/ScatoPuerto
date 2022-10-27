72ae0beed0033c9da491c38bf56864271eabda5b
gsian <gsian@baufest.com>
2022-10-27 12:40:20 -0300

    Fix LineUp
    Fix LineUp - No permite modificar posición de buque

--- Molinos.Scato.WebPuerto/src/app/modulos/lineup/embarque/lineup-embarque.component.ts
+++ Molinos.Scato.WebPuerto/src/app/modulos/lineup/embarque/lineup-embarque.component.ts
@@ -82,12 +82,8 @@ export class LineupEmbarqueComponent implements OnInit {
     this.posicionesDeLineUps = Array.from({ length: this.embarquesPuerto.length }, (v, k) => k + 1);
     this.lineUpService.obtenerListadoUbicacionDeBuquePuerto().subscribe(res => { 
       this.ubicacionDeBuquePuerto = res;
+      this.listadoUbicacionDeBuquePuerto = res.map(u => u.nombre);
     });
-
-    this.lineUpService.obtenerListadoUbicacionDeBuquePuerto()
-      .subscribe(res => {
-        this.listadoUbicacionDeBuquePuerto = res.map(u => u.nombre);
-      });
   }
 
   counter(i: number) {
@@ -324,9 +320,9 @@ export class LineupEmbarqueComponent implements OnInit {
     }
 
   }
-
+  
   nombreUbicacionDeBuquePuerto(numero): string {
-    return numero > 0 && numero != null && this.ubicacionDeBuquePuerto != null && this.ubicacionDeBuquePuerto.find(x => x.id == numero) != undefined ? this.ubicacionDeBuquePuerto.find(x => x.id == numero).nombre.toString() : '';
+    return numero > 0 && numero != null && this.ubicacionDeBuquePuerto != null && this.ubicacionDeBuquePuerto.find(x => x.orden == numero) != undefined ? this.ubicacionDeBuquePuerto.find(x => x.orden == numero).nombre.toString() : '';
   }
 
   numeroUbicacionDeBuquePuerto(nombre): number {
@@ -462,60 +458,60 @@ export class LineupEmbarqueComponent implements OnInit {
     reader.onload = () => {
         console.log(reader.result);
     };
-}
-
-addFileToSave(){
-  if(this.fileToUpload == null){
-    this.confirmationDialogService.confirm('Atención','No has seleccionado ningún archivo.', 'Cerrar', '', null, null, Tipoalerta.Warning);
-    return;
   }
 
-  if(!this.nombreArchivo[0] || this.nombreArchivo[0].tipoArchivo == undefined || this.nombreArchivo[0].tipoArchivo == ""){
-    this.confirmationDialogService.confirm('Atención','No has seleccionado un tipo de archivo.', 'Cerrar', '', null, null, Tipoalerta.Warning);  
-    return;
-  }
-  
-  if (this.fileToUpload.name  == "" ){
-    this.confirmationDialogService.confirm('Atención','El archivo no tiene nombre.', 'Cerrar', '', null, null, Tipoalerta.Warning);  
-    return;
-  }
-
-  if(this.fileToUpload.size >= 5000000){  
-    this.confirmationDialogService.confirm('Atención','El tamaño del archivo debe ser menor a 5MB.', 'Cerrar', '', null, null, Tipoalerta.Warning);
-    return;
-  }    
-
-  
-  const reader = new FileReader();
-  reader.readAsDataURL(this.fileToUpload);
-  reader.onload = () => {
-    var file = reader.result.toString(); 
-      if(file.includes("data:image") || file.includes("openxmlformats") || file.includes("data:application/pdf") || file.includes("text/plain")){
-
-        let fileToAdd = new ArchivoPuerto;
-
-        fileToAdd.archivo = file;
-        fileToAdd.fecha = new Date;
-        fileToAdd.embarque_Id = this.instanciaWorkflow.embarque.id;
-        fileToAdd.nombreArchivo = this.fileToUpload.name        
-        fileToAdd.tipoArchivoPuerto = this.nombreArchivo[0];
-        fileToAdd.id = 0;
-
-        this.ArchivosPuerto.push(fileToAdd);
-        this.ArchivosPuerto = this.ArchivosPuerto.sort((a,b) => a.id - b.id)
-        this.nombreArchivo = null;
-        this.fileToUpload = null
-  
-    }else{
-      //Salgo y no lo dejo agregar 
-      this.confirmationDialogService.confirm('Atención','El archivo tiene un formato inválido.', 'Cerrar', '', null, null, Tipoalerta.Warning);
+  addFileToSave(){
+    if(this.fileToUpload == null){
+      this.confirmationDialogService.confirm('Atención','No has seleccionado ningún archivo.', 'Cerrar', '', null, null, Tipoalerta.Warning);
+      return;
     }
-  }
+
+    if(!this.nombreArchivo[0] || this.nombreArchivo[0].tipoArchivo == undefined || this.nombreArchivo[0].tipoArchivo == ""){
+      this.confirmationDialogService.confirm('Atención','No has seleccionado un tipo de archivo.', 'Cerrar', '', null, null, Tipoalerta.Warning);  
+      return;
+    }
     
-    //Lo agrego a la lista de existentes, para saber cuales guardar van a ser los que tengan id en 0 o nulo
-    //Primero valido que esté toda la data necesaria completa
-   
-}
+    if (this.fileToUpload.name  == "" ){
+      this.confirmationDialogService.confirm('Atención','El archivo no tiene nombre.', 'Cerrar', '', null, null, Tipoalerta.Warning);  
+      return;
+    }
+
+    if(this.fileToUpload.size >= 5000000){  
+      this.confirmationDialogService.confirm('Atención','El tamaño del archivo debe ser menor a 5MB.', 'Cerrar', '', null, null, Tipoalerta.Warning);
+      return;
+    }    
+
+    
+    const reader = new FileReader();
+    reader.readAsDataURL(this.fileToUpload);
+    reader.onload = () => {
+      var file = reader.result.toString(); 
+        if(file.includes("data:image") || file.includes("openxmlformats") || file.includes("data:application/pdf") || file.includes("text/plain")){
+
+          let fileToAdd = new ArchivoPuerto;
+
+          fileToAdd.archivo = file;
+          fileToAdd.fecha = new Date;
+          fileToAdd.embarque_Id = this.instanciaWorkflow.embarque.id;
+          fileToAdd.nombreArchivo = this.fileToUpload.name        
+          fileToAdd.tipoArchivoPuerto = this.nombreArchivo[0];
+          fileToAdd.id = 0;
+
+          this.ArchivosPuerto.push(fileToAdd);
+          this.ArchivosPuerto = this.ArchivosPuerto.sort((a,b) => a.id - b.id)
+          this.nombreArchivo = null;
+          this.fileToUpload = null
+    
+      }else{
+        //Salgo y no lo dejo agregar 
+        this.confirmationDialogService.confirm('Atención','El archivo tiene un formato inválido.', 'Cerrar', '', null, null, Tipoalerta.Warning);
+      }
+    }
+      
+      //Lo agrego a la lista de existentes, para saber cuales guardar van a ser los que tengan id en 0 o nulo
+      //Primero valido que esté toda la data necesaria completa
+    
+  }
 
 
 }
