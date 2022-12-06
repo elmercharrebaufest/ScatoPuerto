3e8e4be560fcfa11ca338fcf701a1991f5d0595d
Evangelino Miranda <emiranda@baufest.com>
2022-12-06 13:31:21 -0500

    modificacion de tabla de nominacion

--- Molinos.Scato.Database/dbo/Tables/Nominacion.sql
+++ Molinos.Scato.Database/dbo/Tables/Nominacion.sql
@@ -1,8 +1,5 @@
 create table Nominacion(
 Id int,
-NominacionDatoTecnico_Id  int IDENTITY (1, 1) NOT NULL,
-NominacionRecibo_Id int null,
-NominacionDetalleIntervencion_Id int null,
 EnviadoFumigador datetime null,
 EnviadoSurveyor datetime null,
 FechaCreacion datetime ,
@@ -11,9 +8,4 @@ FechaEliminacion datetime,
 Embarque_Id int null,
 CONSTRAINT [PK_dbo.Nominacion] PRIMARY KEY CLUSTERED ([Id] ASC),
 CONSTRAINT [FK_dbo.Nominacion_dbo.Embarque_Embarque_Id] FOREIGN KEY ([Embarque_Id]) REFERENCES [dbo].[Embarque] ([Id]),
-CONSTRAINT [FK_dbo.Nominacion_dbo.Embarque_NominacionDatoTecnico_Id] FOREIGN KEY ([NominacionDatoTecnico_Id]) REFERENCES [dbo].[NominacionDatoTecnico] ([Id]),
-CONSTRAINT [FK_dbo.Nominacion_dbo.Embarque_NominacionRecibo_Id] FOREIGN KEY ([NominacionRecibo_Id]) REFERENCES [dbo].[NominacionRecibo] ([Id]),
-CONSTRAINT [FK_dbo.Nominacion_dbo.Embarque_NominacionDetalleIntervencion_Id] FOREIGN KEY ([NominacionDetalleIntervencion_Id]) REFERENCES [dbo].[NominacionDetalleIntervencion] ([Id]),
-
-
 )
