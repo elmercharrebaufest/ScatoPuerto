ce30de595e4f632bd25f6659914523c2da08b9c4
Evangelino Miranda <emiranda@baufest.com>
2022-12-07 09:42:33 -0500

    prueba primeng

--- Molinos.Scato.WebPuerto/src/app/modulos/programa-embarque/programa-embarque-routing.module.ts
+++ Molinos.Scato.WebPuerto/src/app/modulos/programa-embarque/programa-embarque-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ListadoPruebaPrimengComponent } from "./listado-prueba-primeng/listado-prueba-primeng.component";
 import { ProgramaEmbarqueComponent } from "./programa-embarque.component";
 
 const routes: Routes = [
     {
         path: '',
         component: ProgramaEmbarqueComponent
-    }    
+    },
+    {
+        path: 'listado-primeng',
+        component: ListadoPruebaPrimengComponent
+    },
 ]
 
 @NgModule({
