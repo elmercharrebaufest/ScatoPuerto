c596ef7bd2b1faaf5907d7623b0c1ee81e9f79a4
Evangelino Miranda <emiranda@baufest.com>
2022-12-12 10:37:09 -0500

    Se sube cambios para nueva nominacion

--- Molinos.Scato.WebPuerto/src/app/modulos/programa-embarque/nominacion/nominacion-dato-tecnico/nominacion-dato-tecnico.component.ts
+++ Molinos.Scato.WebPuerto/src/app/modulos/programa-embarque/nominacion/nominacion-dato-tecnico/nominacion-dato-tecnico.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { NominacionParametros } from '@ScatoModels/programa-embarque/nominacion-parametros';
+import { NominacionService } from '@ScatoServicios/programa-embarque/nominacion.service';
 
 @Component({
   selector: 'app-nominacion-dato-tecnico',
   templateUrl: './nominacion-dato-tecnico.component.html',
   styleUrls: ['./nominacion-dato-tecnico.component.css']
 })
-export class NominacionDatoTecnicoComponent implements OnInit {
+export class NominacionDatoTecnicoComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  private _nominacionParametros: NominacionParametros = null;
+
+  constructor(private nominacionService: NominacionService) { 
+    this.asignarNominacionParametros();
+  }
+  ngAfterViewInit(): void {
+  }
 
   ngOnInit(): void {
+  }
+
+  get nominacionParametros(): NominacionParametros {
+    return this._nominacionParametros;
+  }
+  set nominacionParametros(value: NominacionParametros){
+    this._nominacionParametros = value;
+  }
+
+  private asignarNominacionParametros(){
+    this.nominacionService.NominacionParametros.subscribe(parametro =>{
+      
+      const nominacionParametos: NominacionParametros = {
+        nominacion_Id : parametro.nominacion_Id,
+        actualizarDatoTecnico : parametro.actualizarDatoTecnico,
+        actualizarRecibos  : parametro.actualizarRecibos,
+        actualizarIntervenciones : parametro.actualizarIntervenciones};
+      this.nominacionParametros = nominacionParametos;
+    });
   }
 
 }
