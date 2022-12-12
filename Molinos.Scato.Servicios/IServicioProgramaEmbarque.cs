54c02a5b254898ac6767cc7666465472cf114398
Lucas Olivella <lmolivella@baufest.com>
2022-12-12 09:48:58 -0300

    Creacion clases Nuevo Servicio

---
+++ Molinos.Scato.Servicios/IServicioProgramaEmbarque.cs
@@ -0,0 +1,19 @@
+﻿using Molinos.Scato.Dominio.Comandos;
+using Molinos.Scato.Dominio.Consultas;
+using Molinos.Scato.Dominio.Dto;
+using Molinos.Scato.Dominio.Entidades;
+using Molinos.Scato.Dominio.Enums;
+using Molinos.Scato.Dominio.Filtros;
+using Molinos.Scato.Dominio.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ServiceModel;
+
+namespace Molinos.Scato.Servicios
+{
+    [ServiceContract(Namespace = "http://scato.molinos.com.ar")]
+    public interface IServicioProgramaEmbarque
+    {
+    }
+}
