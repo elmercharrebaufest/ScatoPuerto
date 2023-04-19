996ec22d50212118fb257371ed4638fbf3d15c57
Lucas Olivella <lmolivella@baufest.com>
2023-04-19 08:39:33 -0300

    cambios Login

--- Molinos.Scato.WebPuertoApi/Seguridad/ClaimsTransformationHttpModule.cs
+++ Molinos.Scato.WebPuertoApi/Seguridad/ClaimsTransformationHttpModule.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using System;
 using System.IdentityModel.Services;
 using System.Security.Claims;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace Molinos.Scato.WebPuertoApi.Seguridad
@@ -16,7 +18,7 @@ namespace Molinos.Scato.WebPuertoApi.Seguridad
             context.PostAuthenticateRequest += Context_PostAuthenticateRequest;
         }
 
-        void Context_PostAuthenticateRequest(object sender, EventArgs e)
+         void Context_PostAuthenticateRequest(object sender, EventArgs e)
         {
             var context = ((HttpApplication)sender).Context;
 
@@ -36,5 +38,96 @@ namespace Molinos.Scato.WebPuertoApi.Seguridad
                 Thread.CurrentPrincipal = transformedPrincipal;
             }
         }
+
+        private async Task loginAzure()
+        {
+            //  string TenantId = "64912498-cd87-469f-a9e5-44e257a06b60";
+            //  string ClientId = "f18c7be4-4ada-4852-a4db-9ffc444c68fa";
+            //var clientId = "f18c7be4-4ada-4852-a4db-9ffc444c68fa";
+            string clientId = "560b50b4-0d0d-42b9-b6ed-792cba10142c";
+            var tenantId = "64912498-cd87-469f-a9e5-44e257a06b60";
+            var redirectUri = "https://localhost/webpuertoapi";
+
+            string clientIdMOC = "5ab4c0b8-bf3b-42ef-90b9-697559762d72";
+            var tenantIdMOC = "c5d83817-b680-4929-9c3c-407e37ea2678";
+            var redirectUriMOC = "https://localhost/webpuertoapi";
+
+            var scopes = new[] { "user.read" };
+
+            var app = PublicClientApplicationBuilder
+                .Create(clientIdMOC)
+                .WithAuthority($"https://login.microsoftonline.com/{tenantIdMOC}")
+                .WithRedirectUri(redirectUriMOC)
+                .Build();
+
+            var result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+            Console.WriteLine(result.AccessToken);
+        }
+
+        private async Task lofAsync()
+        {
+            if (HttpContext.Current.User != null && HttpContext.Current.User.Identity.IsAuthenticated)
+            { // La solicitud ya ha sido autenticada 
+              // Puede acceder a la identidad del usuario autenticado usando HttpContext.Current.User.Identity
+                return;
+            }
+
+            string clientIdMOC = "5ab4c0b8-bf3b-42ef-90b9-697559762d72";
+            var tenantIdMOC = "c5d83817-b680-4929-9c3c-407e37ea2678";
+            var redirectUriMOC = "https://localhost/webpuertoapi";
+
+            var scopes = new[] { "user.read" };
+
+            var app = PublicClientApplicationBuilder
+                .Create(clientIdMOC)
+                .WithAuthority($"https://login.microsoftonline.com/{tenantIdMOC}")
+                .WithRedirectUri(redirectUriMOC)
+                .Build();
+
+            var result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+  
+
+            string accessToken = result.AccessToken;
+            if (accessToken != null)
+            {
+                //// Check if the access token is still valid
+                //JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                //JwtSecurityToken jwt = handler.ReadJwtToken(accessToken);
+                //if (jwt.ValidTo > DateTime.UtcNow)
+                //{
+                // The access token is still valid ClaimsPrincipal
+            //    ClaimsPrincipal claimsPrincipal = CreateClaimsPrincipal(accessToken);
+            //    HttpContext.Current.User = claimsPrincipal;
+                return;
+                //        }
+                //      }
+            }
+            //string refreshToken = tokenCache.GetRefreshToken();
+            //if (refreshToken != null)
+            //{
+            //    try
+            //    {
+            //        AuthenticationResult result = app.aq.AcquireTokenByRefreshToken(new[] { scope }, refreshToken).ExecuteAsync().Result;
+            //        ClaimsPrincipal claimsPrincipal = CreateClaimsPrincipal(result.AccessToken);
+            //        HttpContext.Current.User = claimsPrincipal;
+            //        return;
+            //    }
+            //    catch (Exception)
+            //    {
+
+            //        throw;
+            //    }
+             
+            //}
+
+        }
+        //private ClaimsPrincipal CreateClaimsPrincipal(string accessToken)
+        //{
+        //    //JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+        //    //JwtSecurityToken jwt = handler.ReadJwtToken(accessToken);
+        //    //ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(
+        //    //    new ClaimsIdentity(jwt.Claims, "Bearer", jwt.Claims.FirstOrDefault(c => c.Type == "name")?.Value, jwt.Claims.FirstOrDefault(c => c.Type == "role")?.Value));
+        //    //return claimsPrincipal;
+        //}
     }
 }
