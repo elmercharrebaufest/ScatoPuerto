aec0048c8400d59cb38a3641a1edbe21748c2138
Mauro Leiva <mleiva@baufest.com>
2023-05-11 12:58:16 -0300

    Se crearon y registraron controles y servicios de Afip Se crearon tablas, entidades y dtos para las caratulas

---
+++ Molinos.Scato.Dominio/Dto/AfipPuerto/AfipCaratulaDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molinos.Scato.Dominio.Dto
+{
+    internal class AfipCaratulaDto
+    {
+        public string Id { get; set; }
+        public string CodigoAduana { get; set; }
+        public string CodigoLugarOperativo { get; set; }
+        public DateTime FechaArribo { get; set; }
+        public DateTime FechaZarpada { get; set; }
+        public string Via { get; set; }
+        public string NombreMedioTransporte { get; set; }
+        public string PuertoDestino { get; set; }
+        public string NumeroViaje { get; set; }
+        public IList<AfipCaratulaItinerarioDto> Itinerario { get; set; }
+    }
+}
---
+++ Molinos.Scato.Dominio/Dto/AfipPuerto/AfipCaratulaItinerarioDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molinos.Scato.Dominio.Dto
+{
+    internal class AfipCaratulaItinerarioDto
+    {
+        public string IdentificadorCaratula { get; set; }
+        public string Puerto { get; set; }
+    }
+}
