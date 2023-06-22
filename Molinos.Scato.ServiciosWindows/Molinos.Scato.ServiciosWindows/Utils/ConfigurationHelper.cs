cf463caaa49813faabfd8765d456e0b819f72ede
Gonzalo Maldonado <gmaldonado@baufest.com>
2023-06-22 19:02:09 -0300

    Creacion de servicio de email [ARMOA005-1321][ARMOA1335]

---
+++ Molinos.Scato.ServiciosWindows/Molinos.Scato.ServiciosWindows/Utils/ConfigurationHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace Molinos.Scato.ServiciosWindows.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+
+    public static class ConfigurationHelper
+    {
+        public static List<TimeSpan> HorariosEjecucion { get; } = ObtenerHorariosEjecucion();
+        public static string UrlApi { get; } = ObtenerUrlApi();
+
+        private static List<TimeSpan> ObtenerHorariosEjecucion()
+        {
+            List<TimeSpan> horariosEjecucion = new List<TimeSpan>();
+            var horariosEjecucionSection = ConfigurationManager.AppSettings;
+
+            for (int i = 0; ; i++)
+            {
+                string key = $"HorariosEjecucion:{i}";
+                string value = horariosEjecucionSection[key];
+
+                if (value == null)
+                {
+                    break;
+                }
+
+                if (TimeSpan.TryParse(value, out TimeSpan horario))
+                {
+                    horariosEjecucion.Add(horario);
+                }
+                else
+                {
+                    // Manejar error en el formato del horario
+                }
+            }
+
+            return horariosEjecucion;
+        }
+
+        private static string ObtenerUrlApi()
+        {
+            return ConfigurationManager.AppSettings["UrlApi"];
+        }
+    }
+}
