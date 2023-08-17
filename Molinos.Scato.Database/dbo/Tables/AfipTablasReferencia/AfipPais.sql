d47e14a290f39b132a85d9aaaf5aca3571cb1341
Mauro Leiva <mleiva@baufest.com>
2023-08-17 18:14:07 -0300

    ARMOA005-1387 Tablas Referencia AFIP
    Se crearon las tablas de referencia necesarios para Comunicación de Embarque

---
+++ Molinos.Scato.Database/dbo/Tables/AfipTablasReferencia/AfipPais.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[AfipPais]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+    [Codigo] NVARCHAR(3) NOT NULL,
+    [Descripcion] NVARCHAR(50) NOT NULL,
+    [VigenciaDesde] INT NOT NULL,
+    [VigenciaHasta] INT NOT NULL,
+    [Pais] NVARCHAR(50) NOT NULL,
+    [Aduana] NVARCHAR(50) NOT NULL
+)
---
+++ Molinos.Scato.Database/dbo/Tables/AfipTablasReferencia/AfipPuntoAduanero.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[AfipPuntoAduanero]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+    Codigo NVARCHAR(3) NOT NULL,
+    Descripcion NVARCHAR(255) NOT NULL
+)
