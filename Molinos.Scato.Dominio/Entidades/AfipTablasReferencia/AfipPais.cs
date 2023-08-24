a0de8c82e768ede05792203df67e81c6cdc79fe6
Mauro Leiva <mleiva@baufest.com>
2023-08-24 08:25:36 -0300

    ARMOA005-1387 Tablas de referencia
    Se añadieron las entidades, dtos y mappeos para las tablas de referencia

---
+++ Molinos.Scato.Dominio/Entidades/AfipTablasReferencia/AfipPais.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Molinos.Scato.Dominio.Entidades
+{
+    public class AfipPais : IIdentificable
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public int VigenciaDesde { get; set; }
+        public int VigenciaHasta { get; set; }
+        public string Pais { get; set; }
+        public string Aduana { get; set; }
+    }
+}
---
+++ Molinos.Scato.WebPuerto/src/app/shared/modelos/afip/tablas-afip.ts
@@ -0,0 +1,22 @@
+interface AfipTabla {
+  id: number;
+  codigo: string;
+  descripcion: string;
+}
+
+interface AfipTablaExtra extends AfipTabla {
+  vigenciaDesde: number;
+  vigenciaHasta: number;
+  pais: number;
+  aduana: string;
+}
+
+export interface AfipTipoEmbalaje extends AfipTabla { }         // TIPEMB_DESC
+export interface AfipPuntoAduanero extends AfipTabla { }        // BUR_DESC
+export interface AfipPuerto extends AfipTablaExtra { }          // POR_PAIS
+export interface AfipPais extends AfipTablaExtra { }            // PAY_PAIS
+export interface AfipTipoDocumento extends AfipTabla { }        // DOCIDE_DESC
+export interface AfipNaturalezaEmbalaje extends AfipTabla { }   // NEB_DESC
+export interface AfipLugarOperativo extends AfipTablaExtra { }  // LOT_ADUA
+export interface AfipCondicionContenedor extends AfipTabla { }  // CONCTD_DESC
+
