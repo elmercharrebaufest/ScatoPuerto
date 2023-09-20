8fb56f6f8ee882c80d441e29f8bea5aec8a803e1
Gonzalo Maldonado <gmaldonado@baufest.com>
2023-09-20 16:46:56 -0300

    Agrego validadores

---
+++ Molinos.Scato.Validadores/Common/FechaFinMayorQueFechaInicioAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class FechaFinMayorQueFechaInicioAttribute : ValidationAttribute
+{
+    private readonly string _fechaInicioPropertyName;
+
+    public FechaFinMayorQueFechaInicioAttribute(string fechaInicioPropertyName)
+    {
+        _fechaInicioPropertyName = fechaInicioPropertyName;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var fechaInicioProperty = validationContext.ObjectType.GetProperty(_fechaInicioPropertyName);
+
+        if (fechaInicioProperty != null)
+        {
+            var fechaInicioValue = (DateTime)fechaInicioProperty.GetValue(validationContext.ObjectInstance, null);
+
+            if (value != null && (DateTime)value <= fechaInicioValue)
+            {
+                return new ValidationResult($"La fecha de fin {validationContext.MemberName} debe ser mayor que la fecha de inicio {_fechaInicioPropertyName}.");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
