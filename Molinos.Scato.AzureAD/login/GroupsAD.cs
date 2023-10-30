f109b7108a55ac91650a1e1473457efc14d3fdbe
Mauro Leiva <mleiva@baufest.com>
2023-10-30 10:16:05 -0300

    Corrección de la obtención de grupos de Azure AD

--- Molinos.Scato.AzureAD/login/GroupsAD.cs
+++ Molinos.Scato.AzureAD/login/GroupsAD.cs
@@ -18,18 +18,16 @@ namespace Molinos.Scato.AzureAD.login
             spClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             spClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             spClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+            spClient.DefaultRequestHeaders.Add("ConsistencyLevel", "eventual"); // Necesario para filtrar los grupos
 
-            var search = "https://graph.microsoft.com/v1.0/me/memberOf?$select=displayName";
+            var search = "https://graph.microsoft.com/v1.0/me/memberOf/microsoft.graph.group?$search=\"displayName:LAD_MOAAPP_PUERTO\"&$select=displayName";
 
             var res2211 = spClient.GetStringAsync(search).Result;
             var prue = JObject.Parse(res2211);
 
             foreach (var item in prue["value"])
             {
-                if (item["displayName"].ToString().Contains("LAD_MOAAPP_PUERTO_"))
-                    gruposAD.Add(item["displayName"].ToString());
-                    //Console.WriteLine(item["displayName"].ToString());
-
+                gruposAD.Add(item["displayName"].ToString());
             }
 
             return gruposAD;
