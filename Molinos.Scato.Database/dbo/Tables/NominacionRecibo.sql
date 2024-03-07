2651aac17d675994436abe275cae22c05ddd610d
BAUNET\mpuca <mpuca@baufest.com>
2024-03-07 09:04:40 -0300

    ARMOA005-1444 - Mostrar notificaciones ante abm de recibo en una nominacion, y fix insert de auditoria.

--- Molinos.Scato.Database/dbo/Tables/NominacionRecibo.sql
+++ Molinos.Scato.Database/dbo/Tables/NominacionRecibo.sql
@@ -39,9 +39,8 @@ CREATE TRIGGER [dbo].[Trigger_NominacionRecibo]
 
         SELECT  @idEmbarque = N.Embarque_Id,
                 @dateDiff = DATEDIFF(SECOND, N.FechaCreacion, GETDATE()) -- Segundos entre la creación de la nominación y el insert de Exportador
-        FROM NominacionRecibo R
-        INNER JOIN Nominacion N ON R.Nominacion_Id = N.Id
-        WHERE R.Id = (SELECT DISTINCT id FROM (SELECT Id FROM deleted UNION SELECT Id FROM inserted) a)
+        FROM Nominacion N
+        WHERE N.Id = (SELECT Nominacion_Id FROM deleted UNION SELECT Nominacion_Id FROM inserted);
 
         SELECT @exportadorPrevio = E.Nombre FROM deleted D INNER JOIN Exportador E ON D.Exportador_Id = E.Id
         SELECT @exportadorNuevo = E.Nombre FROM inserted I INNER JOIN Exportador E ON I.Exportador_Id = E.Id
@@ -61,178 +60,178 @@ CREATE TRIGGER [dbo].[Trigger_NominacionRecibo]
             -- Exportador
             IF ((SELECT Exportador_Id FROM deleted) <> (SELECT Exportador_Id FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Exportador_Id', d.Exportador_Id, i.Exportador_Id, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Exportador_Id', d.Exportador_Id, i.Exportador_Id, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Exportador Recibo N°', i.NumeroRecibo,
-                --        ' (', @exportadorPrevio, ' -> ', @exportadorNuevo, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Exportador Recibo N°', i.NumeroRecibo,
+                        ' (', @exportadorPrevio, ' -> ', @exportadorNuevo, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Formato
             IF ((SELECT Formato FROM deleted) <> (SELECT Formato FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Formato', d.Formato, i.Formato, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Formato', d.Formato, i.Formato, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Formato Recibo N°', i.NumeroRecibo, 
-                --        ' (', d.Formato, ' -> ', i.Formato, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Formato Recibo N°', i.NumeroRecibo, 
+                        ' (', d.Formato, ' -> ', i.Formato, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Cantidad
             IF ((SELECT Cantidad FROM deleted) <> (SELECT Cantidad FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Cantidad', d.Cantidad, i.Cantidad, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Cantidad', d.Cantidad, i.Cantidad, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Cantidad Recibo N°', i.NumeroRecibo, 
-                --        ' (', d.Cantidad, ' -> ', i.Cantidad, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Cantidad Recibo N°', i.NumeroRecibo, 
+                        ' (', d.Cantidad, ' -> ', i.Cantidad, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Unidad
             IF ((SELECT Unidad FROM deleted) <> (SELECT Unidad FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Unidad', d.Unidad, i.Unidad, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Unidad', d.Unidad, i.Unidad, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Unidad Recibo N°', i.NumeroRecibo, 
-                --        ' (', d.Unidad, ' -> ', i.Unidad, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Unidad Recibo N°', i.NumeroRecibo, 
+                        ' (', d.Unidad, ' -> ', i.Unidad, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Ajuste
             IF ((SELECT Ajuste FROM deleted) <> (SELECT Ajuste FROM inserted)) BEGIN
             INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Ajuste', d.Ajuste, i.Ajuste, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'Ajuste', d.Ajuste, i.Ajuste, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Ajuste Recibo N°', i.NumeroRecibo, 
-                --        ' (', d.Ajuste, ' -> ', i.Ajuste, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Ajuste Recibo N°', i.NumeroRecibo, 
+                        ' (', d.Ajuste, ' -> ', i.Ajuste, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Puerto de Carga
             IF ((SELECT PuertoDeCarga FROM deleted) <> (SELECT PuertoDeCarga FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'PuertoDeCarga', d.PuertoDeCarga, i.PuertoDeCarga, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'PuertoDeCarga', d.PuertoDeCarga, i.PuertoDeCarga, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Puerto de carga Recibo N°', i.NumeroRecibo, 
-                --        ' (', d.PuertoDeCarga, ' -> ', i.PuertoDeCarga, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Puerto de carga Recibo N°', i.NumeroRecibo, 
+                        ' (', d.PuertoDeCarga, ' -> ', i.PuertoDeCarga, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Puerto de Descarga
             IF ((SELECT PuertoDeDescarga FROM deleted) <> (SELECT PuertoDeDescarga FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'PuertoDeDescarga', d.PuertoDeDescarga, i.PuertoDeDescarga, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'PuertoDeDescarga', d.PuertoDeDescarga, i.PuertoDeDescarga, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Puerto de descarga Recibo N°', i.NumeroRecibo, 
-                --        ' (', d.PuertoDeDescarga, ' -> ', i.PuertoDeDescarga, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Puerto de descarga Recibo N°', i.NumeroRecibo, 
+                        ' (', d.PuertoDeDescarga, ' -> ', i.PuertoDeDescarga, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Descripcion Bienes
             IF ((SELECT DescripcionesBienes FROM deleted) <> (SELECT DescripcionesBienes FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'DescripcionesBienes', d.DescripcionesBienes, i.DescripcionesBienes, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'DescripcionesBienes', d.DescripcionesBienes, i.DescripcionesBienes, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Descripción de bienes Recibo N°', i.NumeroRecibo), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Descripción de bienes Recibo N°', i.NumeroRecibo), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Recibos por dia
             IF ((SELECT RecibosPorDia FROM deleted) <> (SELECT RecibosPorDia FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'RecibosPorDia', d.RecibosPorDia, i.RecibosPorDia, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'RecibosPorDia', d.RecibosPorDia, i.RecibosPorDia, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Recibos por día Recibo N°', i.NumeroRecibo, 
-                --        ' (', CASE d.RecibosPorDia WHEN 1 THEN 'SI' ELSE 'NO' END, ' -> ', CASE i.RecibosPorDia WHEN 1 THEN 'SI' ELSE 'NO' END, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Recibos por día Recibo N°', i.NumeroRecibo, 
+                        ' (', CASE d.RecibosPorDia WHEN 1 THEN 'SI' ELSE 'NO' END, ' -> ', CASE i.RecibosPorDia WHEN 1 THEN 'SI' ELSE 'NO' END, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Mostrar Destinos
             IF ((SELECT MostrarDestinos FROM deleted) <> (SELECT MostrarDestinos FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'MostrarDestinos', d.MostrarDestinos, i.MostrarDestinos, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'MostrarDestinos', d.MostrarDestinos, i.MostrarDestinos, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Mostrar destinos Recibo N°', i.NumeroRecibo, 
-                --        ' (', CASE d.RecibosPorDia WHEN 1 THEN 'SI' ELSE 'NO' END, ' -> ', CASE i.RecibosPorDia WHEN 1 THEN 'SI' ELSE 'NO' END, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Mostrar destinos Recibo N°', i.NumeroRecibo, 
+                        ' (', CASE d.RecibosPorDia WHEN 1 THEN 'SI' ELSE 'NO' END, ' -> ', CASE i.RecibosPorDia WHEN 1 THEN 'SI' ELSE 'NO' END, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
 
             -- Mostrar Bodegas
             IF ((SELECT MostrarBodegas FROM deleted) <> (SELECT MostrarBodegas FROM inserted)) BEGIN
                 INSERT INTO Auditoria
-                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'MostrarBodegas', d.MostrarBodegas, i.MostrarBodegas, GETDATE()
+                SELECT d.Nominacion_Id, d.id, 'NominacionRecibo', 'MostrarBodegas', d.MostrarBodegas, i.MostrarBodegas, GETDATE(), NULL
                 FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
 
-                --IF (@idEmbarque > 0) BEGIN
-                --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-                --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Mostrar bodegas Recibo N°', i.NumeroRecibo, 
-                --        ' (', CASE d.MostrarBodegas WHEN 1 THEN 'SI' ELSE 'NO' END, ' -> ', CASE i.MostrarBodegas WHEN 1 THEN 'SI' ELSE 'NO' END, ')'), GETDATE() 
-                --    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
-                --END
+                IF (@idEmbarque > 0) BEGIN
+                    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Mostrar bodegas Recibo N°', i.NumeroRecibo, 
+                        ' (', CASE d.MostrarBodegas WHEN 1 THEN 'SI' ELSE 'NO' END, ' -> ', CASE i.MostrarBodegas WHEN 1 THEN 'SI' ELSE 'NO' END, ')'), GETDATE() 
+                    FROM deleted AS d JOIN inserted AS i ON d.Id = i.Id
+                END
             END
         END
         -- INSERT (Excepto que sea en el mismo momento de la creación de la nominación)
         ELSE IF EXISTS (SELECT 1 FROM inserted) AND (@dateDiff > 5) BEGIN
             INSERT INTO Auditoria
-            SELECT Nominacion_Id, id, 'NominacionRecibo', 'Exportador', NULL, NumeroRecibo, GETDATE()
+            SELECT Nominacion_Id, id, 'NominacionRecibo', 'Exportador', NULL, NumeroRecibo, GETDATE(), NULL
             FROM inserted
 
-            --IF (@idEmbarque > 0) BEGIN
-            --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-            --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Nuevo Recibo (', NumeroRecibo, ')'), GETDATE()
-            --    FROM inserted
-            --END
+            IF (@idEmbarque > 0) BEGIN
+                INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Nuevo Recibo (', NumeroRecibo, ')'), GETDATE()
+                FROM inserted
+            END
         END
         -- DELETE
-        ELSE IF EXISTS (SELECT 1 FROM deleted) BEGIN
-            INSERT INTO Auditoria
-            SELECT Nominacion_Id, Id, 'NominacionRecibo', 'Exportador', NumeroRecibo, NULL, GETDATE()
+        ELSE IF EXISTS (SELECT 1 FROM deleted) BEGIN    
+             INSERT INTO Auditoria
+            SELECT Nominacion_Id, Id, 'NominacionRecibo', 'Exportador', NumeroRecibo, NULL, GETDATE(), NULL
             FROM deleted
 
-            --IF (@idEmbarque > 0) BEGIN
-            --    INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
-            --    SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Recibo eliminado (', NumeroRecibo, ')'), GETDATE()
-            --    FROM deleted
-            --END
+            IF (@idEmbarque > 0) BEGIN
+                INSERT INTO NotificacionProgramaDeEmbarque (TipoAlerta, Mensaje, Fecha)
+                SELECT 9, CONCAT('Se ha editado el embarque ', @nombreEmbarque, ' - ', @muelle, ': Recibo eliminado (', NumeroRecibo, ')'), GETDATE()
+                FROM deleted 
+            END
         END
     END
