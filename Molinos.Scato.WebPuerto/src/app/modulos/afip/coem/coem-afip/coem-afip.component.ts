1437b8586d60416e464900ac9f3f1640c553a90f
EXT Leiva, Mauro <mleiva@baufest.com>
2024-04-15 09:48:56 -0300

    Merged PR 361: ARMOA005-1861 Mostrar campo ATA completo en COEM
    ARMOA005-1861 Mostrar campo ATA completo en COEM
    
    - Se aumentó el tamaño del modal
    - Se aumentó el tamaño del campo ATA para mostrar todo el nombre y CUIT
    - Se solucionó un bug visual que provocaba que el listado se abra por detras del modal

--- Molinos.Scato.WebPuerto/src/app/modulos/afip/coem/coem-afip/coem-afip.component.ts
+++ Molinos.Scato.WebPuerto/src/app/modulos/afip/coem/coem-afip/coem-afip.component.ts
@@ -4,7 +4,6 @@ import { EstadoCOEM } from '@ScatoModels/afip/estadoCoem';
 import { AfipMotivoSolicitudCambio } from '@ScatoModels/afip/tablas-afip';
 import { CaratulaAfipService } from '@ScatoServicios/afip/caratula-afip.service';
 import { CoemAfipService } from '@ScatoServicios/afip/coem-afip.service';
-import { TablasAfipService } from '@ScatoServicios/afip/tablas-afip.service';
 import { ConfirmationDialogService } from '@ScatoServicios/confirmation-dialog.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -104,7 +103,7 @@ export class CoemAfipComponent implements OnInit, OnDestroy {
   public editarCoem(historial, modal) {
     this.coemId = historial.id;
     this.coemIdentificador = historial.identificadorCOEM
-    this.modalService.open(modal, { size: 'lg', centered: true, backdrop: 'static', keyboard: false });
+    this.modalService.open(modal, { size: 'xl', centered: true, backdrop: 'static', keyboard: false });
   }
 
   public async eliminarCoem(id: number, identificadorCOEM: string) {
