df296d9c3e3caa019d6ae84b64f95889fe8f6c9f
Oscar De Pierola <odepierola@baufest.com>
2024-05-20 12:23:34 -0500

    update application id qa

--- Molinos.Scato.WebPuerto/src/environments/environment.qa.ts
+++ Molinos.Scato.WebPuerto/src/environments/environment.qa.ts
@@ -4,10 +4,12 @@ export const environment = {
   apiUrl: '/Scato.WebPuertoApi/api/',
   apiAzureUrl: '/Scato.AzureAD/',
   apiGraph: 'https://graph.microsoft.com/v1.0/',
-  clientIdMoa: '94426e34-9e75-401a-9f55-906c5e2c4246',
+  clientIdMoa: 'ac5390bd-7225-4fe0-be95-9d2e5aa582a5',
   clientIdMoc: '5ab4c0b8-bf3b-42ef-90b9-697559762d72',
   redirectUri: 'https://scatopuertoqa.molinosagro.com.ar/Scato.WebPuerto/',
   postLogoutRedirectUri: 'https://scatopuertoqa.molinosagro.com.ar/Scato.WebPuerto/login',
   authorityMoa: 'https://login.microsoftonline.com/790c9737-0b8e-4138-a0f4-819cdc1eb64b',
   authorityMoc: 'https://login.microsoftonline.com/c5d83817-b680-4929-9c3c-407e37ea2678'
 };
+
+
