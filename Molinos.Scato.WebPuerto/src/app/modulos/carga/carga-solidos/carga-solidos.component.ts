ced0a1e0e3a7d62df7a045d25a5c8e2f25a8300e
Evangelino Miranda <emiranda@baufest.com>
2024-08-12 14:42:33 -0500

    Modificacion carga componentes Solido

--- Molinos.Scato.WebPuerto/src/app/modulos/carga/carga-solidos/carga-solidos.component.ts
+++ Molinos.Scato.WebPuerto/src/app/modulos/carga/carga-solidos/carga-solidos.component.ts
@@ -67,7 +67,7 @@ export class CargaSolidosComponent implements OnInit {
     // </ ARMOA005-1988 Dylan Lopez>
   ingresoManualSolido: boolean = false;
   existeFechasPeriodoDeCarga: boolean = false;
-
+  moduloDeCarga = null;
   mostrarTableristaOperando: boolean = false;
   terminaImprimir: boolean = false;
   permisosScato: typeof PermisosScato = PermisosScato;
@@ -153,6 +153,7 @@ export class CargaSolidosComponent implements OnInit {
   cargarModuloCarga() {
     this.moduloCargaService.obtenerModuloDeCarga(this.embarqueSelected.moduloDeCargaId)
       .subscribe(res => {
+        this.moduloDeCarga = res;
         this.enviado = res.enviado;
         this.ingresoManualSolido = res.ingresoManualSolido;
         this.usuarioFinalizacion = res.usuarioFinalizacion;
@@ -336,6 +337,7 @@ export class CargaSolidosComponent implements OnInit {
   }
 
   obtenerInicioCarga(inicioCarga){
+    console.log('entroooo obtenerInicioCarga');
     this.inicioCarga = inicioCarga;
     this.cargarModuloCarga();
   }
