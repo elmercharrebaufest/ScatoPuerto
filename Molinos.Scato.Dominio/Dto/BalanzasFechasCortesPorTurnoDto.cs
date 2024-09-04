6aa2a811ec2eaf24cf7929085f6ef331ff9de2b1
Evangelino Miranda tecse <Evangelino.Mirandatecse@molinosagro.com.ar>
2024-09-04 18:57:45 +0000

    Merged PR 1876: Creación de cortes en varios turnos
    Creacion de cortes en varios turnos

---
+++ Molinos.Scato.Dominio/Dto/BalanzasFechasCortesPorTurnoDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molinos.Scato.Dominio.Dto
+{
+    public class BalanzasFechasCortesPorTurnoDto
+    {
+        public string FechaInicio { get; set; }
+        public string HoraInicio { get; set; }
+        public string FechaCorte { get; set; }
+        public string HoraCorte { get; set; }
+        public TurnoPuertoDto TurnoPuerto { get; set; }
+    }
+    public class BalanzasFechasCortesRegistroDto
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+    }
+}
