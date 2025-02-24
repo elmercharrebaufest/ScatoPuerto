71ff2921c66e6c1c9a83098cdf97ea3f6edfd5a0
Oscar De Pierola <odepierola@baufest.com>
2025-02-24 15:41:53 -0500

    formato de correos

--- Molinos.Scato.WebPuertoApi/Helper/NotificacionInicioCarga.cs
+++ Molinos.Scato.WebPuertoApi/Helper/NotificacionInicioCarga.cs
@@ -51,20 +51,29 @@ namespace Molinos.Scato.WebPuertoApi.Helper
             return $"INICIO - {_embarque.Vapor.Nombre} - {materiales} - {muelles}";
         }
 
+
         private string ObtenerCuerpo()
         {
-            var body = new StringBuilder("<div style=\"font-family: Arial, sans-serif; font-size: 14px;\"><table style=\"border-collapse: collapse; width: 100%;\">");
-            body.AppendLine("<thead><tr><th style=\"width:150px; text-align:left; border: 1px solid black;\">INICIO BUQUE</th>");
-            body.AppendLine($"<th style=\"text-align:left; border: 1px solid black;\">{_embarque.Vapor.Nombre}</th></tr></thead><tbody>");
+            var body = new StringBuilder("<div style=\"font-family: Arial, sans-serif; font-size: 14px;\">");
+            body.AppendLine("<table>");
+            body.AppendLine("<thead>");
+            body.AppendLine("<tr>");
+            body.AppendLine("<th style=\"border: 1px solid black; padding: 8px; text-align: left; width: 150px;\">INICIO BUQUE</th>");
+            body.AppendLine($"<th style=\"border: 1px solid black; padding: 8px; text-align: left;\">{_embarque.Vapor.Nombre}</th>");
+            body.AppendLine("</tr>");
+            body.AppendLine("</thead>");
+            body.AppendLine("<tbody>");
 
             var valores = ObtenerValores();
 
             foreach (var val in valores)
             {
-                body.AppendLine($"<tr><td style=\"border: 1px solid black;\">{val.Key}</td><td style=\"border: 1px solid black;\">{val.Value}</td></tr>");
+                body.AppendLine($"<tr><td style=\"border: 1px solid black; padding: 8px; text-align: left;\">{val.Key}</td><td style=\"border: 1px solid black; padding: 8px; text-align: left;\">{val.Value}</td></tr>");
             }
 
-            body.AppendLine("</tbody></table></div>");
+            body.AppendLine("</tbody>");
+            body.AppendLine("</table>");
+            body.AppendLine("</div>");
 
             return body.ToString();
         }
