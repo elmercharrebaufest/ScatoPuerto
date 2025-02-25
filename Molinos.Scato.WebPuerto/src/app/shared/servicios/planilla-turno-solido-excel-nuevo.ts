3f7d5574da3d6072b2f56f9234246e946a87dcde
EXT Leiva, Mauro <Mauro.Leiva@molinosagro.com.ar>
2025-02-25 13:23:37 +0000

    Merged PR 2940: ARMOA005-2547 Corrección en Planilla turnos solidos
    ARMOA005-2547 Corrección en Planilla turnos solidos
    
    Se arregló un problema que impedía la exportación del excel cuando se encontraban turnos sin cargas, bajas cargas o cortes

--- Molinos.Scato.WebPuerto/src/app/shared/servicios/planilla-turno-solido-excel-nuevo.ts
+++ Molinos.Scato.WebPuerto/src/app/shared/servicios/planilla-turno-solido-excel-nuevo.ts
@@ -786,6 +786,7 @@ export class PanillaTurnoSolidoExcelNuevoService {
     }
   }
 
+
   private formatFechaHora(fecha: Date): string {
     const dia = String(fecha.getDate()).padStart(2, '0');
     const mes = String(fecha.getMonth() + 1).padStart(2, '0');
