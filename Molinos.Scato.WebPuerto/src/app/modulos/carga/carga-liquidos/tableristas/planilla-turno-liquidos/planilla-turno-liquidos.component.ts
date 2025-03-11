ec4f44b44f6821d4ceb338ee27723d6f4e359425
BAUNET\mpuca <mpuca@baufest.com>
2025-03-11 11:57:03 -0300

    ARMOA005-2556, 2558, FIX corregir msj al guardar turno general, y permitir editar cortes y bajas cargadas aun no guardadas en bd. Fix bug null ref exception

--- Molinos.Scato.WebPuerto/src/app/modulos/carga/carga-liquidos/tableristas/planilla-turno-liquidos/planilla-turno-liquidos.component.ts
+++ Molinos.Scato.WebPuerto/src/app/modulos/carga/carga-liquidos/tableristas/planilla-turno-liquidos/planilla-turno-liquidos.component.ts
@@ -95,7 +95,7 @@ export class PlanillaTurnoLiquidosComponent implements OnInit {
   turnoModal: any;
   tipoModal: string;
   tituloModal: string = '';
-
+  idsNuevos = 0;  
   constructor(
     private _builder: FormBuilder,
     private _modalService: NgbModal,
@@ -849,7 +849,6 @@ export class PlanillaTurnoLiquidosComponent implements OnInit {
             this.formCorteBajaCarga.get('recordatorio').patchValue(false); 
             this.formCorteBajaCarga.get('horaFin').patchValue(null);
           }
-    
       }
 
       this._modalService.open(modal, { windowClass: 'window-modal-corte', backdropClass: 'modal-corte' }).result
@@ -861,6 +860,11 @@ export class PlanillaTurnoLiquidosComponent implements OnInit {
   async guardarCorteBajaCargaLiquido(dia, turno, modal: any) {
     this.formCorteBajaCarga.markAllAsTouched();
     const rawValue = this.formCorteBajaCarga.getRawValue();
+
+    if(rawValue.id == null){
+     rawValue.id = - 1 - this.idsNuevos;
+      this.idsNuevos ++;
+    }
 
     if (this.tipoModal == 'bajaCarga' && rawValue.tipoLineaEmbarque == null) {
       this.formCorteBajaCarga.controls['tipoLineaEmbarque'].setErrors({ 'incorrect': true });
@@ -897,8 +901,6 @@ export class PlanillaTurnoLiquidosComponent implements OnInit {
       return;
     }
 
- 
-
     if (horaFin < horaTurnoInicio) bErrorFechas = true;
     if (horaFin > horaTurnoFin) bErrorFechas = true;
     if (bErrorFechas) {
@@ -909,12 +911,14 @@ export class PlanillaTurnoLiquidosComponent implements OnInit {
     const cortesFormArray = this.getCorteTurnos(dia, turno);
     // Si existe un corte con recordatorio, éste no puede ser otro ni tampoco puede tener un horario mayor al recordatorio.
     const inicioRecordatorioPrevio = cortesFormArray.controls.find(fg => fg.get('recordatorio').value)?.get('horaInicio').value as string;
-    if (!rawValue.id && inicioRecordatorioPrevio && (esRecordatorio || horaInicio >= inicioRecordatorioPrevio || horaFin >= inicioRecordatorioPrevio)) {
+    if (rawValue.id<0 && inicioRecordatorioPrevio && (esRecordatorio || horaInicio >= inicioRecordatorioPrevio || horaFin >= inicioRecordatorioPrevio)) {
       this.confirmationDialogService.alertar('Existen cortes con recordatorios previos, verifique por favor');
       return;
     }
 
-    const accion = !rawValue.id ? 'agregar' : 'editar';
+    const index = cortesFormArray.controls.findIndex(fg => fg.get('id').value == rawValue.id);
+
+    const accion = index != -1 ? 'editar' : 'agregar';
     const confirmed = await this.confirmationDialogService.confirmar('Planilla de Liquido', `¿Esta seguro de querer ${accion} ${this.tipoModal == 'corte' ? 'un corte' : 'una baja carga'} en la hora indicada?`);
     if (!confirmed) {
       return;
@@ -922,8 +926,7 @@ export class PlanillaTurnoLiquidosComponent implements OnInit {
 
     const corteBajaCargaFormGroup = this.initCorteBajaCarga(rawValue);
 
-    if (rawValue.id) {
-      const index = cortesFormArray.controls.findIndex(fg => fg.get('id').value == rawValue.id);
+    if (index != -1) {
       cortesFormArray.at(index).patchValue(rawValue);
     } else {
       cortesFormArray.push(corteBajaCargaFormGroup);
@@ -1447,8 +1450,8 @@ export class PlanillaTurnoLiquidosComponent implements OnInit {
             this.bGrabandoTurnoActivo = false;
             this.moduloCargaService.actualizarPlanillaLiquido = true;
             this.moduloCargaService.guardarTurnoPlanillaDeTurnos(planillaTurno, this.idModuloDeCarga, enviado, false).subscribe(res => {
-              const guardadoPorTablerista = Turno.guardadoPorTablerista['value'] ? true : false;
-              let mensajeGuardado = guardadoPorTablerista ? 'Sus cambios se enviaron a Recibidores' : 'Se guardaron los cambios en el turno correctamente';
+              //const guardadoPorTablerista = Turno.guardadoPorTablerista['value'] ? true : false;
+              let mensajeGuardado = 'Se guardaron los cambios en el turno correctamente';
               mensajeGuardado = enviado ? 'El turno fue enviado a Recibidores' : mensajeGuardado;
               this.confirmationDialogService.confirm('¡Atención!', mensajeGuardado, 'Aceptar', '', null, null, Tipoalerta.Success);
               if (enviado)
@@ -1456,6 +1459,7 @@ export class PlanillaTurnoLiquidosComponent implements OnInit {
 
               this.recargarTurnosPlanilla();
               this.bGrabandoTurnoActivo = true;
+              this.idsNuevos = 0;
             }, error => {
               console.log(error);
               this.bGrabandoTurnoActivo = true;
