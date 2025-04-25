a3f15a8249eb633bbc168472ea49f4d218aa0c53
Mauro Leiva <mleiva@baufest.com>
2025-04-25 08:12:45 -0300

    PSP-379-1 Fix de autenticación para Servicio Windows
    - Se corrigió un error de texto en la autorización que provocaba que no funcionara

--- Molinos.Scato.ServiciosWindows/Molinos.Scato.ServiciosWindows/EmailService.cs
+++ Molinos.Scato.ServiciosWindows/Molinos.Scato.ServiciosWindows/EmailService.cs
@@ -76,7 +76,7 @@ namespace Molinos.Scato.ServiciosWindows
                 try
                 {
                     var authHeader = ConfigurationHelper.AuthHeader;
-                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("basic", authHeader);
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authHeader);
                     HttpResponseMessage response = await client.GetAsync(apiUrl);
 
                     if (response.IsSuccessStatusCode)
