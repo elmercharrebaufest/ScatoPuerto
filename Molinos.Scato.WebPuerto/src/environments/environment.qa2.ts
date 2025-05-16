82df233b78dc316ecd9d3d39b63aba57d9961e48
Mauro Leiva <mleiva@baufest.com>
2025-05-16 12:47:17 -0300

    Añadidos parametros de entorno para QA2

--- Molinos.Scato.WebPuerto/src/environments/environment.qa2.ts
+++ Molinos.Scato.WebPuerto/src/environments/environment.qa2.ts
@@ -1,15 +1,17 @@
 export const environment = {
   production: true,
   envName: 'qa2',
-  apiUrl: '/QA2/Scato.WebPuertoApi/api/',
+  apiUrl: '/Scato.WebPuertoApi/api/',
   apiAzureUrl: '/Scato.AzureAD/',
   apiGraph: 'https://graph.microsoft.com/v1.0/',
   clientIdMoa: 'ac5390bd-7225-4fe0-be95-9d2e5aa582a5',
   clientIdMoc: '5ab4c0b8-bf3b-42ef-90b9-697559762d72',
-  redirectUri: 'https://scatopuertoqa.molinosagro.com.ar/QA2/Scato.WebPuerto',
-  postLogoutRedirectUri: 'https://scatopuertoqa.molinosagro.com.ar/QA2/Scato.WebPuerto/login',
+  redirectUri: 'https://scatopuertoqa2.molinosagro.com.ar/Scato.WebPuerto',
+  postLogoutRedirectUri: 'https://scatopuertoqa2.molinosagro.com.ar/Scato.WebPuerto/login',
   authorityMoa: 'https://login.microsoftonline.com/790c9737-0b8e-4138-a0f4-819cdc1eb64b',
-  authorityMoc: 'https://login.microsoftonline.com/c5d83817-b680-4929-9c3c-407e37ea2678'
+  authorityMoc: 'https://login.microsoftonline.com/c5d83817-b680-4929-9c3c-407e37ea2678',
+  webPuertoApiUsername:'WebPuertoApi',
+  webPuertoApiPassword:'ipidsadkjdW3QB7B'
 };
 
 
