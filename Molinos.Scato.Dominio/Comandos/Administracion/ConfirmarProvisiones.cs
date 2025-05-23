24b05650ce48e8e526e9e9496dfa9bb79916ec77
BAUNET\mpuca <mpuca@baufest.com>
2025-05-23 10:29:17 -0300

    Desarrollo consulta, confirmacion y guardado de provisiones

---
+++ Molinos.Scato.Dominio/Comandos/Administracion/ConfirmarProvisiones.cs
@@ -0,0 +1,14 @@
+﻿using Molinos.Scato.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molinos.Scato.Dominio.Comandos.Administracion
+{
+    public class ConfirmarProvisiones: Comando
+    {
+        public List<int> IdsTarifas { get; set; }
+    }
+}
