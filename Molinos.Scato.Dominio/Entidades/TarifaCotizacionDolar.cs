5e54b1bdacb17f65b45d05f4cc1ddfa2686b2770
Franco Maieli <franco.maieli@molinosagro.com.ar>
2026-02-05 19:40:41 -0300

    PSP-596: creacion de tabla en proyecto de base de datos, entidad, dto y modificaciones en AdministracionController y ServicioAdministracion agregando ObtenerTarifaDolar, ObtenerPeriodosDisponibles y GuardarTarifaDolar

---
+++ Molinos.Scato.Dominio/Entidades/TarifaCotizacionDolar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Molinos.Scato.Dominio.Entidades
+{
+	public class TarifaCotizacionDolar : IIdentificable
+	{
+		[Key]
+		public virtual int Id { get; set; }
+
+		public virtual DateTime Periodo { get; set; }
+
+		public virtual decimal ValorDolar { get; set; }
+
+		public virtual DateTime FechaActualizacion { get; set; }
+
+		public virtual string UsuarioActualizacion { get; set; }
+	}
+}
