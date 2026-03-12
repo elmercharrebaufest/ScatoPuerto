b9da150f3a6b6ee295272006d2b16a8781f1cda8
Mauro Leiva <mleiva@baufest.com>
2026-03-12 17:21:36 -0300

    PSP-582 Seleccionar nuevos muelles en pantallas
    - Se añadieron los nuevos muelles a los combos de muelles de administracion y porgrama de embarque
    - Se adapto el funcionamiento de las busquedas para que funcionen correctamente

--- Molinos.Scato.Repositorio/ConsultasEF/ListarEmbarquesAdministracionConsulta.cs
+++ Molinos.Scato.Repositorio/ConsultasEF/ListarEmbarquesAdministracionConsulta.cs
@@ -5,6 +5,7 @@ using Molinos.Scato.Dominio.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Globalization;
 using System.Linq;
 
 namespace Molinos.Scato.Repositorio.ConsultasEF
@@ -174,9 +175,15 @@ namespace Molinos.Scato.Repositorio.ConsultasEF
                         Desamarre = g.SelectMany(x => x.LineUp?.ModuloDeCarga?.ModuloDeCargaPeriodoDeCarga ?? Enumerable.Empty<ModuloDeCargaPeriodoDeCarga>())
                                         .FirstOrDefault()?.FechaDesamarro,
                         Muelle = g.Key.SanBenito ? "San Benito" :
-                                     g.Key.Vicentin ? "Vicentin" :
-                                     g.Key.Noryon ? "Nouryon" :
-                                     g.Key.OtrosMuelles ? g.Key.OtroMuelleNombre :
+                                 g.Key.Vicentin ? "Vicentin" :
+                                 g.Key.Noryon ? "Nouryon" :
+                                 g.Key.OtrosMuelles ? g.Key.OtroMuelleNombre :
+                                 n.Nominacion?.NominacionDatoTecnico?.Muelle != null ?
+                                     (n.Nominacion.NominacionDatoTecnico.Muelle.Descripcion == "Otros Muelles" ?
+                                         n.Nominacion.NominacionDatoTecnico.OtroMuelleNombre :
+                                         n.Nominacion.NominacionDatoTecnico.Muelle.Descripcion) :
+                                 n.Nominacion?.NominacionDatoTecnico?.MuelleDeCarga?.Descripcion == "Otros Muelles" ?
+                                     n.Nominacion.NominacionDatoTecnico.OtroMuelleNombre :
                                      n.Nominacion?.NominacionDatoTecnico?.MuelleDeCarga?.Descripcion ?? "",
                         Cliente = string.Join(",",
                                 n.Nominacion?.NominacionDatoTecnico?.NominacionDatoTecnicoCoordinadorPuerto?
@@ -220,7 +227,15 @@ namespace Molinos.Scato.Repositorio.ConsultasEF
 
             if (muelles != null && muelles.Any())
             {
-                queryList = queryList.Where(x => x.ItemsEmbarque.Any(item => muelles.Contains(item.Muelle))).ToList();
+                bool compararSinTildes(string a, string b) => string.Compare(a, b, CultureInfo.InvariantCulture, CompareOptions.IgnoreNonSpace | CompareOptions.IgnoreCase) == 0;
+
+                var muellesConocidos = new[] { "San Benito", "Vicentin", "Nouryon", "Zárate", "Bahía Blanca", "Necochea" };
+                var incluirOtrosMuelles = muelles.Any(m => compararSinTildes(m, "Otros Muelles"));
+
+                queryList = queryList.Where(x => x.ItemsEmbarque.Any(item => 
+                    muelles.Any(m => compararSinTildes(m, item.Muelle)) ||
+                    (incluirOtrosMuelles && !muellesConocidos.Any(conocido => compararSinTildes(conocido, item.Muelle)))
+                )).ToList();
             }
 
             if (exportadores != null && exportadores.Any())
