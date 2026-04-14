810dd0f67b2b6dbf1b5602890656ce63b604ea12
Fredy Claros Rojas <fclaros@baufest.com>
2026-04-14 11:18:21 -0300

    PSP-649 Ejecución de Scripts

--- Molinos.Scato.Database/dbo/Tables/Exportador.sql
+++ Molinos.Scato.Database/dbo/Tables/Exportador.sql
@@ -3,6 +3,8 @@
     [Nombre] NVARCHAR (60) NULL,    
     [Almacen_Id] INT NULL, 
     [Habilitado] BIT NOT NULL DEFAULT 1, 
+    [Cuit] VARCHAR(50) NULL, 
+    [IdSap] VARCHAR(50) NULL, 
     CONSTRAINT [PK_dbo.Exportador] PRIMARY KEY CLUSTERED ([Id] ASC),
 	CONSTRAINT [UK_Exportador_Nombre] UNIQUE (Nombre),
 	CONSTRAINT [FK_dbo.Exportador_dbo.Almacen_Almacen_Id] FOREIGN KEY ([Almacen_Id]) REFERENCES [dbo].[Almacen] ([Id])
