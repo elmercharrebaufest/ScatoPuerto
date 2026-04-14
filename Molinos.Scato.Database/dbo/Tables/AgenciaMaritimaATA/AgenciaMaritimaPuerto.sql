8b320b60883fab8e5cabfeb3fec2a520184d9e0d
Fredy Claros Rojas <fclaros@baufest.com>
2026-04-14 11:18:21 -0300

    PSP-649 Ejecución de Scripts

--- Molinos.Scato.Database/dbo/Tables/AgenciaMaritimaATA/AgenciaMaritimaPuerto.sql
+++ Molinos.Scato.Database/dbo/Tables/AgenciaMaritimaATA/AgenciaMaritimaPuerto.sql
@@ -3,7 +3,10 @@
     [Nombre] NVARCHAR (60) NOT NULL,    
     [Cuit] NVARCHAR(50) NULL, 
     [Activa] BIT NOT NULL DEFAULT 1, 
+    [IdAta] INT NULL, 
+    [IdSap] VARCHAR(50) NULL, 
     CONSTRAINT [PK_dbo.AgenciaMaritimaPuerto] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [UK_AgenciaMaritimaPuerto_Nombre] UNIQUE (Nombre)
+    CONSTRAINT [UK_AgenciaMaritimaPuerto_Nombre] UNIQUE (Nombre),
+    CONSTRAINT [FK_AgenciaMaritimaPuerto_ATAPuerto] FOREIGN KEY ([IdAta]) REFERENCES [dbo].[ATAPuerto] ([Id])
 );
 GO
--- Molinos.Scato.Database/dbo/Tables/Destino.sql
+++ Molinos.Scato.Database/dbo/Tables/Destino.sql
@@ -2,8 +2,11 @@
     [Id]     INT            IDENTITY (1, 1) NOT NULL,
     [Nombre] NVARCHAR (60) NULL,
     [Activo] BIT NOT NULL DEFAULT 1, 
-
+    [IdSap] VARCHAR(50) NULL, 
+    [Nacionalidad] VARCHAR(150) NULL, 
+    [IdBandera] INT NULL, 
     CONSTRAINT [PK_dbo.Destino] PRIMARY KEY CLUSTERED ([Id] ASC),
-	CONSTRAINT [UK_Destino_Nombre] UNIQUE (Nombre) 
+	CONSTRAINT [UK_Destino_Nombre] UNIQUE (Nombre),
+    CONSTRAINT [FK_Destino_Bandera] FOREIGN KEY ([IdBandera]) REFERENCES [dbo].[Bandera] ([Id])
 );
 GO
