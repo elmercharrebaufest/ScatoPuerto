d1401a6cdedc241f2b4c287508ea47f1d0746040
Fredy Claros Rojas <fclaros@baufest.com>
2026-04-15 12:13:12 -0300

    PSP-649 se corrige entidades y script

--- Molinos.Scato.Database/dbo/Tables/CoordinadorPuerto.sql
+++ Molinos.Scato.Database/dbo/Tables/CoordinadorPuerto.sql
@@ -2,7 +2,7 @@
     [Id]     INT            IDENTITY (1, 1) NOT NULL,
     [Nombre] NVARCHAR (60) NOT NULL,    
     [Habilitado] BIT NOT NULL DEFAULT 1, 
-    [IdSap] VARCHAR(50) NULL, 
+    [CodigoSap] VARCHAR(50) NULL, 
     CONSTRAINT [PK_dbo.CoordinadorPuerto] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [UK_CoordinadorPuerto_Nombre] UNIQUE (Nombre)
 );
