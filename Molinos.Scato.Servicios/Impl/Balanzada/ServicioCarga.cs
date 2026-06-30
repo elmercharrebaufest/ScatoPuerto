38dae9406d4aca3c00e34bc2c54dd5a92e6267cd
Franco Maieli <fmaieli@baufest.com>
2026-06-30 16:42:53 -0300

    PSP-729: se agrega parametro de configuracion para envio a SAP de balanzadas y modificacion en ServicioCarga para habilitar o no el envio segun dicho parametro

--- Molinos.Scato.Servicios/Impl/Balanzada/ServicioCarga.cs
+++ Molinos.Scato.Servicios/Impl/Balanzada/ServicioCarga.cs
@@ -599,7 +599,11 @@ namespace Molinos.Scato.Servicios.Impl
         {
             if (!enviadoASap && cargaInicial != null)
             {
-                _colaComandos.Encolar(new EnviarLecturaBalanzadaTransmisionASap { Id = idBalanzada, NumeroBalanza = numeroBalanza });
+                var parametro = _repositorio.ObtenerPrimero<Parametros>(p => p.Descripcion == "ConfiguracionEnvioSAPBalanzadas");
+                if (parametro != null && parametro.Activo && parametro.Parametro1)
+                {
+                    _colaComandos.Encolar(new EnviarLecturaBalanzadaTransmisionASap { Id = idBalanzada, NumeroBalanza = numeroBalanza });
+                }
             }
         }
 
