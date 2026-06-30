7dd9aa00957496eaba510bdf3f7e2c52ea1f5d30
BAUNET\paimar <paimar@baufest.com>
2026-06-30 14:47:09 -0300

    Se agrega logueo de errores

--- Molinos.Scato.WebPuertoApi/Controllers/CamarasController.cs
+++ Molinos.Scato.WebPuertoApi/Controllers/CamarasController.cs
@@ -1,6 +1,7 @@
 using Molinos.Scato.Dominio.Seguridad;
 using Molinos.Scato.Servicios;
 using Molinos.Scato.WebPuertoApi.Atributos;
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -19,8 +20,20 @@ namespace Molinos.Scato.WebPuertoApi.Controllers
         [Route("api/Camaras/Listar")]
         public HttpResponseMessage Listar()
         {
-            var camaras = servicio.ListarCamarasAduana();
-            return Request.CreateResponse(HttpStatusCode.OK, camaras);
+
+            try
+            {
+                var camaras = servicio.ListarCamarasAduana();
+                return Request.CreateResponse(HttpStatusCode.OK, camaras);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(
+                    HttpStatusCode.InternalServerError,
+                    ex);
+            }
+            //var camaras = servicio.ListarCamarasAduana();
+            //return Request.CreateResponse(HttpStatusCode.OK, camaras);
         }
     }
 }
